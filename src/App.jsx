b0ce7c9b9b9d0be4808041681ae9ba17ce8d6018
Meenal Kesarkar <159876692+MeenalKesarkar@users.noreply.github.com>
2026-07-15 20:39:12 +0530

    Add Footer component and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter } from 'react-router-dom';
 import Header from './Header';
+import Footer from './Footer';
 import './App.css'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
    <BrowserRouter>
       <Header />
+      <Footer />
    </BrowserRouter>
   )
 }
