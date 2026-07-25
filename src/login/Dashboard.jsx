0d5161fbe86e62d5e4260289d7c5f2bab3721ef5
Meenal Kesarkar <159876692+MeenalKesarkar@users.noreply.github.com>
2026-07-25 23:56:26 +0530

    Fix case of login.css filename

--- src/login/Dashboard.jsx
+++ src/login/Dashboard.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./login.css";
+import "./Login.css";
 import api from "../../service/api";
 import studentImages from "../studentImages";
 function Dashboard() {
