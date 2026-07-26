0ff57f4bc0593ec056d278f637e49b4d02594227
Meenal Kesarkar <159876692+MeenalKesarkar@users.noreply.github.com>
2026-07-26 18:57:31 +0530

    New active backend api url added

--- service/api.js
+++ service/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://college-s-sports-club.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
